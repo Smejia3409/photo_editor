112fc452443a67cf250368dfe696a4ab23e8ee42
Sandro Mejia <sandromejia3409@gmail.com>
2023-02-15 23:31:50 -0500

    added manipulation to edited pics

--- photo.py
+++ photo.py
@@ -2,40 +2,57 @@ from PIL import Image, ImageEnhance, ImageFilter
 import os
 
 
-def save_photo(filename, img):
+def temp_save(filename, img):
     clean_name = os.path.splitext(filename)[0]
-    img.save(f"./editedImgs/{clean_name}_edited.jpeg")
+    # print(f"./editedImgs/{clean_name}.jpeg")
+    img.save(f"./editedImgs/{clean_name}.jpeg")
+
+# gets file clean file name
+
+
+def clean_name(filename):
+    clean_name = os.path.splitext(filename)[0]
+    return clean_name
 
 
 class Photo():
     def __init__(self, path, filename):
         # self.path = path
         # self.filename = filename
-        self.img = Image.open(f"{path}/{filename}")
+
         # edited = Image.open(
         #     f"./editedImgs/{os.path.splitext(filename)[0]}_editied.jpeg")
 
         # logic setup for path and filename
-        if not os.path.isfile(f"./editedImgs/{os.path.splitext(filename)[0]}_editied.jpeg"):
+        if os.path.isfile(f"./editedImgs/{os.path.splitext(filename)[0]}.jpeg"):
 
             self.path = "./editedImgs"
-            self.filename = f"{filename}_editied.jpeg"
+            self.filename = f"{clean_name(filename)}.jpeg"
+            self.img = Image.open(f"{self.path}/{self.filename}")
+
             print("file found in edited")
+
         else:
             self.path = path
             self.filename = filename
+            self.img = Image.open(f"{path}/{filename}")
+
+            print("img not found")
 
     def sharpen(self):
+        print(self.path)
         edit = self.img.filter(ImageFilter.SHARPEN)
-        save_photo(self.filename, edit)
+        temp_save(self.filename, edit)
         print(self.filename + ":sharpened")
 
     def greyscale(self):
+        print(self.path)
         edit = self.img.convert("L")
-        clean_name = os.path.splitext(self.filename)[0]
-        edit.save(f"./editedImgs/{clean_name}_editied.jpeg")
+        print(self.filename + ":Greyscale")
+        temp_save(self.filename, edit)
 
     def rotate(self):
+        print(self.path)
         edit = self.img.rotate(-90)
-        save_photo(self.filename, edit)
+        temp_save(self.filename, edit)
         print(self.filename + ":rotated")
